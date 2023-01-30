e00c32136a8d793ece8d6008c9d3a6cfb821f061
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-30 17:41:37 +0530

    Basic exception handling using try catch & finally

---
+++ Calc.cs
@@ -0,0 +1,31 @@
+﻿namespace Basic_Exception_Handling
+{
+    internal class Calc
+    {
+        public static void Division()
+        {
+
+            Console.WriteLine("Please enter two numbers to devide");
+            int x = int.Parse(Console.ReadLine());
+            int div = int.Parse(Console.ReadLine());
+            try
+            {
+                div = x / div;
+               
+            }
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine("DivideByZeroException");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Finally Block");
+            }
+            Console.WriteLine($"Result is {div}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Basic_Exception_Handling
+{
+
+    class MyClass
+
+    {
+        public static void Main(string[] args)
+        {
+            Calc.Division();
+        }
+    }
+}
